7d1989ba28955589b99aa2eef42ccae3cca95acf
Petar Petkov <petar.b.petkov@gmail.com>
2014-12-14 06:56:55 -0600

    questions model functions and controllers (testing calls) added

--- controllers/Questions.php
+++ controllers/Questions.php
@@ -11,21 +11,27 @@ class Questions extends Controller {
 
     public function index() {
         $this->view->title = 'questions/index go to view';
+        //testing for aside tags and categories
+        //$this->model->getAllTags();
+        //$this>model->getAllCategories();
         $this->view->render();
     }
 
     public function view() {
         $this->view->title = 'questions/view';
+        $this->model->getAllQuestions(); //testing for all question
         $this->view->render();
     }
 
     public function category() {
         $this->view->title = 'questions/category';
+        $this->model->getAllQuestions(array("category" => 2, "tag" => 0)); //testing for categories
         $this->view->render();
     }
 
     public function tag() {
         $this->view->title = 'questions/tag';
+        $this->model->getAllQuestions(array("category" => 0, "tag" => 3)); //testing for tags
         $this->view->render();
     }
 
--- models/Questions_Model.php
+++ models/Questions_Model.php
@@ -7,4 +7,24 @@ class Questions_Model extends Model
     {
         parent::__construct($c);
     }
+    
+    public function getAllQuestions($params = array("category" => 0, "tag" => 0)){  //default values of cat and tag might be better to be changed to null
+        //var_dump($outputArray); // debug with this
+        return $outputArray;
+    }
+    
+    public function getAllCategories(){
+        //numberOfQuestionsInCategory($categoryId) // call this function for each category found to get it`s number of questions
+        return $outputArray;
+    }
+    
+    public function getAllTags(){
+        //gett all tags and sort them by number of usees
+        return $outputArray;
+    }
+    
+// private functions. You can`t call them from outside of this model
+    private function numberOfQuestionsInCategory($categoryId){
+        return $numberOfQuestions;
+    }
 }
