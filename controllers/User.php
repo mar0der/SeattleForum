4a0fc4ba515eda0a27013ac707634bc1356b9644
Petar Petkov <petar.b.petkov@gmail.com>
2014-12-14 06:14:44 -0600

    user model and controller functions added (no logic yet)

--- controllers/User.php
+++ controllers/User.php
@@ -16,16 +16,26 @@ class User extends Controller {
 
     public function register() {
         $this->view->title = 'user/register form';
+        $this->model->editUser("username","password", "afaf@faf.com", "realname", "male", "avatar"); //this will create user
         $this->view->render();
     }
 
     public function edit() {
         $this->view->title = 'user/edit profile';
+        $this->model->editUser(1, "password", "afaf@faf.com", "realname", "male", "avatar"); //this will call edit function and edit user 1 
         $this->view->render();
     }
 
     public function profile() {
         $this->view->title = 'view/profile';
+        $this->model->viewUser(1); //this will get the details for user 1
+        $this->view->render();
+    }
+    
+    public function delete() {
+        //add credentials check
+        $this->view->title = 'view/profile';
+        $this->model->deleteUser(1); //this delete user 1
         $this->view->render();
     }
 
--- models/User_Model.php
+++ models/User_Model.php
@@ -7,4 +7,24 @@ class User_Model extends Model
     {
         parent::__construct($c);
     }
+    
+    public function addUser($username, $password, $email, $realName="", $gender="", $avatar=""){
+        //use var_dump for debugging//use var_dump for debugging
+        return $isDone;
+    }
+    
+    public function editUser($userId, $password = "", $email="", $realName = "", $gender="", $avatar=""){
+        //use var_dump for debugging
+        return $isDone;
+    }
+    
+    public function viewUser($userId){
+        // var_dump($outputArra); //use this row to check if the returnet result is what you expect
+        return $outputArray;
+    }
+    
+    public function deleteUser($userId){
+        //use var_dump for debugging
+        return $isDone;
+    }
 }
