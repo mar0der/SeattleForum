96f2ece6da973df4fd6095a3cceef9ef0411b59e
Petar Petkov <petar.b.petkov@gmail.com>
2014-12-14 08:11:10 -0600

    question model functions and controllers (testing calls) added

--- controllers/Question.php
+++ controllers/Question.php
@@ -16,15 +16,26 @@ class Question extends Controller {
 
     public function view() {
         $this->view->title = 'question/view';
+        $this->model->getQuestion($questionId);
         $this->view->render();
     }
 
     public function ask() {
         $this->view->title = 'question/ask';
+        $this->model->addQuestion($questionId, $creatorId, $body, $tags);
         $this->view->render();
     }
 
     public function edit() {
+        $this->view->title = 'question/edit';
+        $this->model->editQuestion($questionId);
+        //$this->model->saveEditedQuestion($questionId, $body); // use this for testint this function
+        //$this->model->deleteQuestion($questionId); // test this function with this row
+        $this->view->render();
+    }
+
+//testing purposes to handle the exeption in order to be able to have non action functions in the controller
+    private function ask2() {
         $this->view->title = 'question/edit';
         $this->view->render();
     }
--- models/Question_Model.php
+++ models/Question_Model.php
@@ -1,10 +1,38 @@
 <?php
 
-class Question_Model extends Model 
-{
+class Question_Model extends Model {
 
-    public function __construct($c) 
-    {
+    public function __construct($c) {
         parent::__construct($c);
+    }
+
+    public function getQuestion($questionId) {
+        //getCreator($creatorId); // use this function to get the creator data
+        return $outputArray;
+    }
+    
+    public function addQuestion($questionId, $creatorId, $body, $tags){
+        $isDone = false;
+        //insert all the stuff into the corrct tables
+        return $isDone;
+    }
+
+    public function editQuestion($questionId){
+        return $outputArray;
+    }
+    
+    public function saveEditedQuestion($questionId, $body){
+        $isDone = false;
+        return $isDone;
+    }
+    
+    public function deleteQuestion($questionId){
+        $isDone = false;
+        return $isDone;
+    }
+    
+//############ private functions ##############
+    private function getCreator($creatorId){
+        return $outputArray;
     }
 }
