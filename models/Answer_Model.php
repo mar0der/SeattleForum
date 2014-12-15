f1e03cc634d2807c6686a42dd4502371346362ab
bbaychev <mb_int@abv.com>
2014-12-15 19:09:30 +0200

    Models update (Question, Answers)
    The models for Question and Answers have been updated with all select
    statements needed. The other statements need to be looked into more.
    
    The Questions model has had a few column name changes, so they're
    properly pointing to the appropriate field.

--- models/Answer_Model.php
+++ models/Answer_Model.php
@@ -2,9 +2,42 @@
 
 class Answer_Model extends Model 
 {
-
     public function __construct($c) 
     {
         parent::__construct($c);
+    }
+
+    public function addAnswer($questionId, $creatorId, $answerBody) {
+        //???
+    }
+
+    public function editAnswer($answerId) {
+        $answer_edit = $this->db->select("SELECT A.id as answer_id, A.body as answer_body
+FROM answers A WHERE A.id = :ansId", array(':ansId' => $answerId));
+        return $answer_edit;
+    }
+
+    public function saveEditedAnswer($answerId, $answerBody) {
+
+    }
+
+    public function deleteAnswer($answerId) {
+
+    }
+
+    //private functions
+    private function getAllAnswersForQuestion($questionId) {
+        $result = $this->db->select("SELECT A.id as answer_id, A.body as answer_body, A.edit_date, A.creator_id
+FROM answers A WHERE A.question_id = :qId", array(':qId' => $questionId));
+
+        foreach($result as $key => $val) {
+            $result[$key]["creator"] = $this->getAnswerCreator($result[$key]["creator_id"]);
+        }
+    }
+
+    private function getAnswerCreator($creatorId) {
+        $creator = $this->db->select("SELECT A.avatar, A.userid as user_id, A.username
+FROM users A INNER JOIN answers B ON A.userid = B.creator_id WHERE B.creator_id = :creatorId", array(':creatorId' => $creatorId));
+        return $creator;
     }
 }
