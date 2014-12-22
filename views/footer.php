57ef7e2f5aeeade16e7eb18580f8d45c3dadde41
Petar Petkov <petar.b.petkov@gmail.com>
2014-12-22 15:45:58 -0600

    html 5 validadion icon added

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,4 @@
         </main>
-        <footer><?php echo Config::getValue('siteName')." &copy; ".date('Y');?></footer>
+        <footer><?php echo Config::getValue('siteName')." &copy; ".date('Y');?><br /><a href="http://validator.w3.org/check?uri=http%3A%2F%2Fforum.petarpetkov.com%2Fquestions" target="_blank"><img src="/images/icons/w3c-valid-html.jpg" width = "60" alt = "HTML 5 validations" /></a></footer>
     </body>
 </html>
