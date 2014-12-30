16108bf5b4ceb7ad7d6778fc59ed0776aa453854
Petar Petkov <petar.b.petkov@gmail.com>
2014-12-30 10:39:45 -0600

    config.inc.php added back to commit list until i have other contributors helping

--- config/config.inc.php
+++ config/config.inc.php
@@ -1,13 +1,16 @@
 <?php
+
 //PATHS AND DEFAULT FILES
-$c['siteName'] = 'Seattle Forum';
+$c['siteName'] = 'Seattle Forum - a StackOverflow like forum!';
 $c['paths']['url'] = 'http://forum.petarpetkov.com/';
-$c['paths']['avatarFolder'] = '/var/www/seattleforum/public/images/avatars/';
-$c['paths']['avatarUrl'] = $c['paths']['url'].'images/avatars/';
-$c['paths']['libs'] = '../libs/';
-$c['paths']['controllers'] = '../controllers/';
-$c['paths']['models'] = '../models/';
-$c['paths']['views'] = '../views/';
+$c['paths']['appFolder'] = '/var/www/seattleforum/';
+$c['paths']['avatarFolder'] = $c['paths']['appFolder'] . 'public/images/avatars/';
+$c['paths']['avatarUrl'] = $c['paths']['url'] . 'images/avatars/';
+$c['paths']['aclConfig'] = $c['paths']['appFolder'] . 'config/Acl.conf.php';
+$c['paths']['libs'] = $c['paths']['appFolder'] . 'libs/';
+$c['paths']['controllers'] = $c['paths']['appFolder'] . 'controllers/';
+$c['paths']['models'] = $c['paths']['appFolder'] . 'models/';
+$c['paths']['views'] = $c['paths']['appFolder'] . 'views/';
 $c['errorFile'] = 'Error.php';
 $c['defaultFile'] = 'Index.php';
 //DB CONFIGURATION
@@ -15,6 +18,6 @@ $c['database']['type'] = 'mysql';
 $c['database']['host'] = 'localhost';
 $c['database']['dbname'] = 'seattleForum';
 $c['database']['username'] = 'seattleForum';
-$c['database']['password'] = '';
+$c['database']['password'] = '123';
 //hashes
 $c['salt'] = 'oDI;-A=qM_SF7}h/Eih5Z0SQ{X^Zpc24ezlQbJoq|VhRw[ARZ+}t,^w:Xen`o[n%';
