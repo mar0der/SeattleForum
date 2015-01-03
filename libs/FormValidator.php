368929553f08c6da52bc8b0b1711da81791eefd5
Petar Petkov <petar.b.petkov@gmail.com>
2015-01-03 12:50:34 -0600

    More work on form validation class. Added default and user defined validation for email, password and username

--- libs/FormValidator.php
+++ libs/FormValidator.php
@@ -3,6 +3,7 @@
 class FormValidator {
 
     public function username($username = '', $params = '') {
+        //change this logic to make parameters required. This is unsafe now!
         if ($params != '') {
             $pattern = $params['pattern'];
             $minLength = $params['minLength'];
@@ -10,12 +11,13 @@ class FormValidator {
         } else {
             $minLength = 4;
             $maxLength = 18;
-            $pattern = "/.{" . $minLength . "," . $maxLength . "}/";
+            $pattern = "/^[^!@#$%^&*()\-+=\\\/\'\"|?<>]{".$minLength.",".$maxLength."}$/";
         }
         if (preg_match($pattern, $username)) {
             return true;
         } else {
-            return false;
+            //improve the messaging system. Take it from parameters
+            return "The username contains some illegal characters (!.@,%,^,&,*,(, ) etc..";
         }
     }
 
@@ -27,31 +29,35 @@ class FormValidator {
         } else {
             $minLength = 6;
             $maxLength = 20;
-            $pattern = "/.{" . $minLength . "," . $maxLength . "}/";
+            $pattern = "/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?!.*\s).{".$minLength.",".$maxLength."}$/";
         }
         if (preg_match($pattern, $password)) {
             return true;
         } else {
-            return false;
+            return "Psword must be between ".$minLength." and ".$maxLength. "and to contain at least one capital letter, small letter and digit";
         }
     }
 
     public function email($email = '', $params = '') {
         if ($params != '') {
             $pattern = $params['pattern'];
+            if (preg_match($pattern, $email)) {
+                return true;
+            } else {
+                return "Invalid email address!";
+            }
         } else {
-            $pattern = "/./";
-        }
-        if (preg_match($pattern, $email)) {
-            return true;
-        } else {
-            return false;
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                return true;
+            }else{
+                return "Invalid email address!";
+            }
         }
     }
 
     public function image($image = '', $params = '') {
-        //put some lohic here
- 
+        //put some logic here
+
         return true;
     }
 
